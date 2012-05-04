57dec3e451de9c481e49f9ff6c0a9e96fa39828b
bendavies <bendavies@he3.com.au>
2012-05-04 22:26:06 +1000

    Added bootstrap data

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,17 @@
+import screentime.Page
+
 class BootStrap {
 
     def init = { servletContext ->
+		
+		//check whether the test data already exists
+		if(!Page.count()) {
+			
+			new Page(pageName:"Google",pageUrl:"http://www.google.com",pageOrder:1).save(failOnError:true)
+			new Page(pageName:"Yahoo",pageUrl:"http://www.yahoo.com",pageOrder:2).save(failOnError:true)
+			
+		}
+		
     }
     def destroy = {
     }
