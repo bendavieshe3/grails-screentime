e97aa94afb9402be5451dd2a3a3057fe1b781e6e
bendavies <bendavies@he3.com.au>
2012-06-14 00:17:49 +1000

    Updated list of default pages

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -7,11 +7,11 @@ class BootStrap {
 		//check whether the test data already exists
 		if(!Page.count()) {
 			
-			new Page(pageName:"Brisbane Radar",pageUrl:"http://www.bom.gov.au/products/IDR663.loop.shtml",pageOrder:1).save(failOnError:true)
-			new Page(pageName:"Brisbane Forecast",pageUrl:"http://www.bom.gov.au/qld/forecasts/brisbane.shtml",pageOrder:2).save(failOnError:true)
-			
-			new Page(pageName:"HTTP Status Codes", pageUrl:"http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html", pageOrder:3).save(failOnError:true)
-			new Page(pageName:"Learning Technical Stuff", pageUrl:"http://www.learningtechnicalstuff.com", pageOrder:4).save(failOnError:true)	
+			new Page(pageName:"JQuery Mobile",pageUrl:"http://jquerymobile.com",pageOrder:1).save(failOnError:true)
+			new Page(pageName:"Grails",pageUrl:"http://grails.org",pageOrder:2).save(failOnError:true)
+			new Page(pageName:"Spine JS", pageUrl:"http://spinejs.com", pageOrder:3).save(failOnError:true)
+			new Page(pageName:"Grails Jira Task Board", pageUrl:"http://jira.grails.org/secure/TaskBoard.jspa?type=TB&selectedProjectId=10020&selectedBoardId=13125&start=0", pageOrder:4).save(failOnError:true)
+			new Page(pageName:"Jenkins CI", pageUrl:"http://ci.jenkins-ci.org/view/All/", pageOrder:5).save(failOnError:true)
 		}
 		
     }
